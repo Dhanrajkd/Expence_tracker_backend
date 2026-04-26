2e6d27aa858391208b9d960d4970d8e600f973dd
Dhanrajkd <dhanrajd158@gmail.com>
2026-04-26 18:05:47 +0530

    updated project

--- index.js
+++ index.js
@@ -9,22 +9,15 @@ import Addbudject from "./Routes/Budjectrouter.js"
 const port=4000
 const app=express()
 
-/* app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true,
-  allowedHeaders: ["Content-Type", "Authorization"],
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
-})) */
-
   app.use(cors())
 
 app.use(express.json())
 app.get("/",(req,res)=>{
     console.log("server is running")
 })
-app.listen(port,(req,res)=>{
-    console.log(`server running on${port}`)
-})
+app.listen(port, "0.0.0.0", () => {
+   console.log(`Server running on port ${port}`);
+});
 mongoose.connect("mongodb+srv://dhanrajd158_db_user:Expensetracker@cluster0.d879r9f.mongodb.net")
 .then(()=>console.log("Db connected"))
 .catch((err)=>console.log(err))

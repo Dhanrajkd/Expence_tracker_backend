59b3e0865054fa1b0ac5ca294107f405fad73a36
Dhanrajkd <dhanrajd158@gmail.com>
2026-05-08 21:24:26 +0530

    updated project

--- Controllers/Transactioncontroller.js
+++ Controllers/Transactioncontroller.js
@@ -4,6 +4,10 @@ export const addtransaction=async(req,res)=>{
     try{
         const id=req.user.id
         console.log("add_transaction",req.body)
+        const {amount,paymentfor,method}=req.body
+        if(!amount || !paymentfor || !method){
+            res.status(404).json({success:false,message:"all fields required"})
+        }
         const data=new Transaction({
             amount:req.body.amount,
             paymentfor:req.body.paymentfor,
@@ -24,16 +28,36 @@ export const addtransaction=async(req,res)=>{
 export const get_trans_data=async(req,res)=>{
     try{
        const id=req.user.id
+       const { month } = req.query
+       console.log("API HIT");
+       console.log("month",month)
         const transactions = await Transaction.find({userid:id})
-        console.log("trans",transactions)
         const page=parseInt(req.query.page) || 1
         console.log("page",page)
         const limit=parseInt(req.query.limit) || 5
         const skip=(page-1)*limit
-         const totalrecords=await Transaction.countDocuments({userid:id})
-         const data=await Transaction.find({userid:id})
-         .skip(skip)
-        .limit(limit)
+        const startDate = new Date(`${month}-01`);
+            const [year, m] = month.split("-");
+            const nextMonth = new Date(year, m); 
+            const data = await Transaction.find({
+                userid: id,
+                createdAt: {
+                $gte: startDate,
+                $lt: nextMonth
+                }
+            } )
+            .skip(skip)
+            .limit(limit);
+            const totalrecords=await Transaction.find({
+                 userid: id,
+                createdAt: {
+                $gte: startDate,
+                $lt: nextMonth
+                }
+            })
+            console.log("data", data);
+            console.log("totalrecords",totalrecords)
+            console.log("records",Math.ceil(totalrecords.length/limit))
          const expences=await Transaction.aggregate([
             {
                 $match:{
@@ -50,7 +74,11 @@ export const get_trans_data=async(req,res)=>{
         const piedata=await Transaction.aggregate([
             {
                 $match:{
-                    userid:new mongoose.Types.ObjectId(id)
+                    userid:new mongoose.Types.ObjectId(id),
+                     createdAt: {
+                    $gte: startDate,
+                    $lt: nextMonth
+                    }
                 }
             },
             {
@@ -67,21 +95,24 @@ export const get_trans_data=async(req,res)=>{
                 }
             }
         ])
-        console.log("piedata",piedata)
-        console.log("data",data)
+        console.log(data)
         const totalexpences=expences[0].totalexpences
         if(!transactions){
            return res.status(404).json({success:false,message:"No data"})
         }
+        if(!data){
+              return res.status(404).json({success:false,message:"No data"})
+        }
         res.status(200).json(
             {success:true,
+                transactions,
                 data,
                 totalexpences,
                 piedata,
                 page,
                 limit,
                 totalrecords,
-                totalpages:Math.ceil(totalrecords / limit),
+                totalpages:Math.ceil(totalrecords.length / limit),
             })
     }
     catch(err){
